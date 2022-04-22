24acd1024207547ae63769a69b41706488e34e5d
Unesty <unesty@protonmail.com>
2022-04-22 11:20:27 +0300

    something working

--- src/gameplay.c
+++ src/gameplay.c
@@ -47,6 +47,24 @@ char inpfd;
 //// end globals
 //////////////////////////////////////////////
 
+/**
+ * Cross-platform sleep function for C
+ * @param int milliseconds
+ */
+void sleep_ms(int milliseconds)
+{
+    #ifdef WIN32
+        Sleep(milliseconds);
+    #elif _POSIX_C_SOURCE >= 199309L
+        struct timespec ts;
+        ts.tv_sec = milliseconds / 1000;
+        ts.tv_nsec = (milliseconds % 1000) * 1000000;
+        nanosleep(&ts, NULL);
+    #else
+        usleep(milliseconds * 1000);
+    #endif
+}
+
 void io_init() {
     input = mmap(0, 1, PROT_READ|PROT_WRITE, MAP_SHARED, inpfd, 0);
 }
@@ -67,3 +85,4 @@ int main() {
     io_init();
     gameplay_loop();
 }
+
